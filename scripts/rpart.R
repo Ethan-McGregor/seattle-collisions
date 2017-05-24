a6608331e31d3bcf118b3e3480f515a5b4562174
Ethan-McGregor <ethanm4@uw.edu>
2017-05-23 20:11:45 -0700

    updated rpart

--- scripts/rpart.R
+++ scripts/rpart.R
@@ -4,23 +4,23 @@ library('shiny')
 library('leaflet')
 library('stringr')
 library('dplyr')
-
+library('randomForest')
 col <- read.csv('../data/SDOT_Collisions.csv')
 
 
-data = data.frame(col$INJURIES,col$DISTANCE,col$VEHCOUNT)
+data = data.frame(col$INJURIES,col$DISTANCE,col$VEHCOUNT,col$PERSONCOUNT, col$ST_COLCODE)
 
 #sample <- sample_n(data, 500)
 
-
+data = na.roughfix(data)
 dt = sort(sample(nrow(data), nrow(data)*.7))
 trainindex <- sample(data, trunc(length(data)/2))
 train<-data[dt,]
 test<-data[-dt,]
 
-class = rpart(col.INJURIES ~ col.DISTANCE + col.VEHCOUNT , 
-                     data = train)
 
+class <- randomForest(col.INJURIES ~ col.DISTANCE + col.VEHCOUNT + col.PERSONCOUNT + col.ST_COLCODE, 
+                     data = train, importance = TRUE,  ntree = 100)
 
 
 pre <- predict(class,test) 
