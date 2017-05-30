5ea30010e405131424dbc9d9eb1720e8d2bdc2a9
Sean <sean10base@gmail.com>
2017-05-30 16:25:45 -0700

    re-add map.R

---
+++ scripts/INFO 201 map.R
@@ -0,0 +1,30 @@
+library(shiny)
+library(leaflet)
+library(plotly)
+library(dplyr)
+library(knitr)
+
+
+data <- read.csv('data/SDOT_Collisions.csv')
+
+# remove missing data
+acc.data <- na.omit(data)
+
+# find most popular month
+freq <- as.data.frame(table(acc.data$INCDTTM))
+
+month.day = sub('/([^/]*)$', '', freq[,1])
+month.day <- as.data.frame(table(month.day))
+month.data <-  gsub( "/.*$", "", freq[,1] )
+month <- as.data.frame(table(month.data))
+
+# graph and components
+bar_graph <- plot_ly(month, x = month$month.data, y = month$Freq, type = 'bar',
+                   marker = list(color = c('black', 'blue', 'black', 'black', 'black',
+'black', 'black', 'black', 'black', 'black', 'black', 'black'))) %>% 
+  layout(title = "Collisions by month",
+         xaxis = list(title = "Month number"),
+         yaxis = list(title = "Total collisions occured"))
+
+# display graph
+bar_graph
