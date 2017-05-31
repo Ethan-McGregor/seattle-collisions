2bfbb8947760bb0771d42c797e26826881e8d024
Katherine <kathlu@uw.edu>
2017-05-31 14:59:54 -0700

    more progress on weather graph

--- scripts/weather.R
+++ scripts/weather.R
@@ -21,12 +21,12 @@ WeatherGraph <- function(data) {
   colnames(df.wide) <- get.col.names
   
   # display graph 
-  bar.graph <- plot_ly(df.wide, x = ~WEATHER, y = ~Fatality.Collision, type = "bar", marker = list(color = ~"cyan"), 
-                       name = "Fatality Collision", hoverinfo = "text", text = ~paste(WEATHER, "<br>", Fatality.Collision, "Fatality Collisions")) %>%
+  bar.graph <- plot_ly(df.wide, x = ~WEATHER, y = ~Property.Damage.Only.Collision, name = "Property Damage Collision", marker = list(color = "navy"), type = "bar", 
+                       name = "Property Damage Only Collision", hoverinfo = "text", text = ~paste(WEATHER, "<br>", Property.Damage.Only.Collision, "Property Damage Only Collisions")) %>%
     add_trace(y = ~Injury.Collision, name = "Injury Collision", marker = list(color = "slateblue"),
               hoverinfo = "text", text = ~paste(WEATHER, "<br>", Injury.Collision, "Injury Collisions")) %>% 
-    add_trace(y = ~Property.Damage.Only.Collision, name = "Property Damage Collision", marker = list(color = "navy"),
-              hoverinfo = "text", text = ~paste(WEATHER, "<br>", Property.Damage.Only.Collision, "Property Damage Only Collisions")) %>% 
+    add_trace(y = ~Fatality.Collision, name = "Fatality Collision", marker = list(color = ~"cyan"),
+              hoverinfo = "text", text = ~paste(WEATHER, "<br>", Fatality.Collision, "Fatality Collisions")) %>% 
     add_trace(y = ~Serious.Injury.Collision, name = "Serious Injury Collision", marker = list(color = "blue"),
               hoverinfo = "text", text = ~paste(WEATHER, "<br>", Serious.Injury.Collision, "Serious Injury Collisions")) %>% 
     layout(title = "Weather & Car Collision Severity", xaxis = list(title = "Weather"),
@@ -35,6 +35,3 @@ WeatherGraph <- function(data) {
   return(bar.graph)
 
 }
-
-plot.data <- collisions %>% filter(ROADCOND == "Wet")
-WeatherGraph(plot.data)
